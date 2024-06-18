4a36b273b0316236d0bebd9ce51e16ecc1f5b281
inayatuizzati <159796022+inayatuizzati@users.noreply.github.com>
2024-06-18 17:38:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 $(document).ready(function () {
-  $('.left-curtain').css('width', '0%');
-  $('.right-curtain').css('width', '0%');
+  $(function () {
+    var flame = $('#flame');
+    var txt = $('h1');
 
-  $('.love-letter').click(function () {
-    $('.envelope').css({ animation: 'fall 3s linear 1', '-webkit-animation': 'fall 3s linear 1' });
-    $('.envelope').fadeOut(800, function () {
-      $('.love-letter .heart, .love-letter .text, .love-letter .front').hide();
-
-      $('#card').css({ visibility: 'visible', opacity: 0, transform: 'scale(0.1)' });
-      $('#card').animate(
-        { opacity: 1 },
-        {
-          duration: 1000,
-          step: function (now, fx) {
-            var scale = 1 + Math.sin(now * Math.PI) * 0.1;
-            $(this).css('transform', 'scale(' + scale + ')');
+    flame.on({
+      click: function () {
+        flame.removeClass('burn').addClass('puff');
+        $('.smoke').each(function () {
+          $(this).addClass('puff-bubble');
+        });
+        $('#glow').remove();
+        txt.hide().html('it will come true...once again happy birthday Mohammad Rivky Taufani, i love you from deep inside my heart❤︎').delay(750).fadeIn(300);
+        $('#candle').animate(
+          {
+            opacity: '.5',
           },
-        }
-      );
+          100
+        );
+      },
     });
   });
 });
